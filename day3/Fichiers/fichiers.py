55b98be329559d97cfaac147bf5b145e2acbe5ab
Olivier Sepulchre <osepulchre@solocal.com>
2025-10-01 14:41:29 +0200

    Fichiers

---
+++ day3/Fichiers/fichiers.py
@@ -0,0 +1,43 @@
+import os
+from collections import Counter
+from threading import Thread
+
+def process_file(file_name:str):
+        print(f"{file_name}: {os.path.getsize(file_path)}")
+        with open(file_path, 'rb') as f:
+            contenu=f.read()
+            compteur = Counter(contenu)
+            print(compteur)
+            f.close()
+
+def process_files():
+    try:
+        file = files.pop()
+        while True:
+            process_file(file)
+            file=files.pop()
+    except IndexError:
+        pass
+
+dir_path="d:\\temp\\"
+
+# lister les fichiers
+files=[]
+for file_name in os.listdir(dir_path):
+    file_path=dir_path+file_name
+    if os.path.isfile(file_path):
+        files.append(file_path)
+
+print(files)
+
+threads=[]
+nb_threads=10
+for i in range(nb_threads):
+    t=Thread(target=process_files)
+    threads.append(t)
+    t.start()
+
+for i in range(nb_threads):
+    t.join()
+
+
