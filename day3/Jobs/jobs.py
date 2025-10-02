49fec7c4241605a02587ad3976e3aa03942c1c87
Olivier Sepulchre <osepulchre@solocal.com>
2025-10-02 09:44:29 +0200

    Jobs: inherit Thread

--- day3/Jobs/jobs.py
+++ day3/Jobs/jobs.py
@@ -12,27 +12,25 @@ class Job:
     def __repr__(self):
         return f"{self.name} ({self.__duration} heures)"
 
-class Worker:
+class Worker(threading.Thread):
     def __init__(self, name):
+        super().__init__()
         self.__name = name
     
-    def __execute(self, jobs):
+    def run(self):
         try:
             while True:
-                job = jobs.pop(0)
+                job = self.__jobs.pop(0)
                 print(f"{self}: début {job}")
                 job.perform()
                 print(f"{self}: fin {job}")
         except IndexError:
             print(f"{self}: fin de la journée")
 
-    def run(self, jobs: list[Job]):
-        self.__thread = threading.Thread(target=self.__execute, args=(jobs,))
-        self.__thread.start()
-
-    def wait(self):
-        self.__thread.join()
-
+    def start(self, jobs):
+        self.__jobs = jobs
+        super().start()
+    
     def __repr__(self):
         return self.__name
 
@@ -41,7 +39,7 @@ jobs = [Job("repassage", 2), Job("vaisselle", 1), Job("cuisine", 2), Job("ménag
 workers = [Worker("Marge"), Worker("Omer"), Worker("Bart"), Worker("Lisa"), Worker("Maggie")]
 
 for worker in workers:
-    worker.run(jobs)
+    worker.start(jobs)
 
 for worker in workers:
-    worker.wait()
+    worker.join()
