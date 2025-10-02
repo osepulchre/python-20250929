0eefc42d6d20b0b803cdf70018565133f75f63de
Olivier Sepulchre <osepulchre@solocal.com>
2025-10-02 09:34:15 +0200

    Queues: inherit Thread + use Queue.task_done()

--- day3/Queues/queues.py
+++ day3/Queues/queues.py
@@ -3,36 +3,35 @@ import queue
 import time
 from threading import Thread
 
-class Producteur:
+class Producteur(Thread):
     def __init__(self, queue):
+        super().__init__()
         self.__queue = queue
         self.__running = False
-        self.__thread = None
     
     def __produire(self):
         value = int(random.uniform(0, 100))
         self.__queue.put(value)
         print(f"posted: {value}")
 
-    def __internal_run(self):
+    def run(self):
         self.__running = True
         while self.__running:
             self.__produire()
             time.sleep(random.uniform(0,2))
 
     def __enter__(self):
-        self.__thread=Thread(target=self.__internal_run)
-        self.__thread.start()
+        self.start()
     
     def __exit__(self, type, value, traceback):
         self.__running = False
         self.__queue.put(-1)
-        self.__thread.join()
+        self.join()
     
-class Consommateur:
+class Consommateur(Thread):
     def __init__(self, queue):
+        super().__init__()
         self.__queue = queue
-        self.__thread = None
         self.__running = False
     
     def __consommer(self):
@@ -41,24 +40,25 @@ class Consommateur:
             self.__running=False
         else:
             print(f"read: {value}")
+        self.__queue.task_done()
     
-    def __internal_run(self):
+    def run(self):
         self.__running = True
         while self.__running:
             self.__consommer()
             time.sleep(random.uniform(0,3))
 
     def __enter__(self):
-        self.__thread=Thread(target=self.__internal_run)
-        self.__thread.start()
+        self.start()
 
     def __exit__(self, type, value, traceback):
-        self.__thread.join()
+        self.join()
 
 
-fifo=queue.SimpleQueue()
+fifo=queue.Queue()
 
 with Consommateur(fifo):
     with Producteur(fifo):
         time.sleep(15)
+
 
